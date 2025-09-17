65fdc0bb304f98e8c430eb561ea6df011988df7d
mohamedamid <amid.mohamed2024@gmail.com>
2025-09-17 16:10:31 +0100

    ajouter code repositorie implement InMemoryUserRepository

--- src/main/java/com/gestionBank/repositorie/impl/InMemoryUserRepository.java
+++ src/main/java/com/gestionBank/repositorie/impl/InMemoryUserRepository.java
@@ -1,4 +1,36 @@
 package com.gestionBank.repositorie.impl;
 
-public class InMemoryUserRepository {
+import com.gestionBank.model.User;
+import com.gestionBank.repositorie.UserRepository;
+import  java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryUserRepository implements UserRepository {
+    private final Map<UUID, User> users = new ConcurrentHashMap<>();
+
+    public User save(User user){
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    public Optional<User> findById(UUID id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return users.values().stream().filter(user -> user.getEmail().equals(email)).findFirst();
+    }
+
+    public List<User> findAll(){
+        return new ArrayList<>(users.values());
+    }
+
+    public void delete (UUID id){
+        users.remove(id);
+    }
+
+    public boolean existsById(String email){
+        return users.values().stream().anyMatch(user -> user.getEmail().equals(email));
+    }
+
 }
