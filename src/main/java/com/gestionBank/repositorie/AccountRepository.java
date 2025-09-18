f5df6ab5c894f4633fd4066dfc85ae0e1cbbf9a8
mohamedamid <amid.mohamed2024@gmail.com>
2025-09-18 11:20:20 +0100

    ajouter le code repositorie AccountRepository

--- src/main/java/com/gestionBank/repositorie/AccountRepository.java
+++ src/main/java/com/gestionBank/repositorie/AccountRepository.java
@@ -1,4 +1,14 @@
 package com.gestionBank.repositorie;
 
+import com.gestionBank.model.Account;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface AccountRepository {
+    Account save(Account account);
+    Optional<Account> findByAccoutId(String accoutId);
+    List<Account> findAll();
+    List<Account> findByOwnerUserId(UUID ownerUserId);
+    void delete(Account account);
 }
